911c46ce7e785637794cdfc3a5a5b2e0d69b1056
Ngoc Tran <binhngoc17@gmail.com>
2015-09-05 18:42:05 +0800

    CIS194 - HW01: Implementation

--- upenn/cis194/HW01.hs
+++ upenn/cis194/HW01.hs
@@ -1,39 +1,45 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW01 where
+import Data.List as List
 
 -- Exercise 1 -----------------------------------------
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit x = x `mod` 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = undefined
+dropLastDigit x = (x `div` 10)
 
 -- Exercise 2 -----------------------------------------
 
 toRevDigits :: Integer -> [Integer]
-toRevDigits = undefined
+toRevDigits x 
+  | x < 10 = [x] 
+  | otherwise = List.reverse((x `mod` 10) : (toRevDigits (x `div` 10)))
 
 -- Exercise 3 -----------------------------------------
 
 -- Double every second number in a list starting on the left.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther (x:(y:zs)) = (x:(2*y: (doubleEveryOther zs)))
 
 -- Exercise 4 -----------------------------------------
 
 -- Calculate the sum of all the digits in every Integer.
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits [] = 0
+sumDigits (x:ys) = List.sum(toRevDigits x) + (sumDigits ys)
 
 
 -- Exercise 5 -----------------------------------------
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = undefined
+luhn x = (sumDigits (doubleEveryOther (List.reverse (toRevDigits x)))) `mod` 10 == 0
 
 -- Exercise 6 -----------------------------------------
 
